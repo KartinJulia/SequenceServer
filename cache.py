e30c17502858dc1ec7f3561e62f654b28be0a0e7
KartinWWW <31469746+KartinJulia@users.noreply.github.com>
2019-10-24 00:32:28 -0700

    cache updated

---
+++ cache.py
@@ -0,0 +1,20 @@
+#from time_est import *
+#from distribute_computation import *
+import time
+
+
+class Cache:
+    def __init__(self,user_id):
+        self._catch = []
+        self._id = user_id
+    def release(self):
+        self._cache = []
+
+    def add(self,worker):
+        self._cache.append(worker)
+
+    def search(self,worker):
+        for i in self._catch:
+            if worker._id == i._id:
+                return i._result
+    
