d1d6463632c971c46ec5b03146770c8cc7248e2b
KartinW <jiatianwang@email.arizona.edu>
2019-11-05 10:23:28 -0700

    Update distribute_computation.py

--- distribute_computation.py
+++ distribute_computation.py
@@ -1,2 +1,20 @@
 import multiprocessing
 import time
+from Worker import Worker
+
+core_count = multiprocessing.cpu_count()
+
+worker_lst = []
+for i in range (core_count):
+    temp_worker = Worker()
+    mul = multiprocessing.Process(target=temp_worker)
+    worker_lst.append(mul)
+
+for w in worker_lst:
+    w.start()
+
+for e in worker_lst:
+    w.join()
+
+for c in range (len(worker_lst)):
+    print("Worker " + str(c) +" is alive: {}".format(worker_lst[c].is_alive())) 
